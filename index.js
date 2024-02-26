a8f96a4e72964d86f1ee641a19fbb1315508e847
Dipesh1Pokharel <pokhreldeepesh02@gmail.com>
2024-02-26 14:34:32 -0500

    new

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const axiosRetry = require('axios-retry');
+// const fetch = require('node-fetch')  ;
+const multer = require('multer');
+const https = require('https'); 
+const app = express();
+const port = 4000;
+
+app.use(cors());
+app.use(bodyParser.json({limit: '1000mb'}));
+
+app.listen(PORT, ()=>{
+    console.log("Server Started");
+})
